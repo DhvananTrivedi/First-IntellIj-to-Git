68f585a95cf5adaf59dd1a41a69446e55782c1a9
dhvanan <dhvanan.trivedi@gmail.com>
2018-02-10 10:51:06 +0530

    Added Class D

---
+++ src/main/java/com/brevitaz/D.java
@@ -0,0 +1,11 @@
+package com.brevitaz;
+
+/**
+ * @author dhvanan on 10/2/18 Saturday
+ * @project first
+ **/
+public class D extends C {
+    void print(){
+        System.out.println(getClass().getName());
+    }
+}
